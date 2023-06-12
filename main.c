d27c92c634cc25da53ef5d4a62a25cea8f1945ee
MiguelAmaro <miguelamaro100@gmail.com>
2023-06-12 00:04:38 -0400

    added timer functions

--- main.c
+++ main.c
@@ -14,7 +14,36 @@ int GetLogicalCoreCount(void)
   uint32_t Result = SystemInfo.dwNumberOfProcessors;
   return Result;
 }
-
+//~Timer
+typedef struct timer timer;
+struct timer
+{
+  uint32_t IsInitialized;
+  uint64_t Frequency;
+};
+timer GlobalTimer = {0};
+void TimerInit(void)
+{
+  LARGE_INTEGER Freq = {0};
+  QueryPerformanceFrequency(&Freq);
+  GlobalTimer.Frequency = Freq.QuadPart;
+  GlobalTimer.IsInitialized = 1;
+}
+uint64_t TimerGetTick(void)
+{
+  LARGE_INTEGER Tick = {0};
+  QueryPerformanceCounter(&Tick);
+  uint64_t Result = Tick.QuadPart;
+  return Result;
+}
+double TimerGetSecondsElepsed(uint64_t StartTick, uint64_t EndTick)
+{
+  if(!GlobalTimer.IsInitialized) TimerInit();
+  uint64_t Delta = EndTick-StartTick;
+  double Result = (Delta*1.0)/(double)GlobalTimer.Frequency;
+  return Result;
+}
+//~Threads
 typedef DWORD threadproc(void *);
 uint64_t ThreadCreate(void *Param, threadproc ThreadProc)
 {
@@ -58,17 +87,31 @@ void ThreadLaunchGroup(uint64_t *ThreadHandles, int ThreadCount, uint64_t *Begin
 int main(void)
 {
   printf("hello from c\n");
+  uint64_t BeginTick = 0;
+  uint64_t EndTick   = 0;
   
+  //CPU
   printf("parallel cpu c\n");
   uint32_t  LogicalCoreCount = GetLogicalCoreCount();
-  uint64_t *ThreadHandles = VirtualAlloc(0, LogicalCoreCount*sizeof(uint64_t), MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
-  uint64_t BeginSignal = (uint64_t)CreateEvent(NULL, TRUE, FALSE, TEXT("Thread Group Begin Signal"));
+  uint64_t *ThreadHandles    = VirtualAlloc(0, LogicalCoreCount*sizeof(uint64_t), MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+  uint64_t  BeginSignal      = (uint64_t)CreateEvent(NULL, TRUE, FALSE, TEXT("Thread Group Begin Signal"));
+  
   ThreadLaunchGroup(ThreadHandles, LogicalCoreCount, &BeginSignal);
+  BeginTick = TimerGetTick();
   ThreadBeginWork(BeginSignal);
   ThreadSync(ThreadHandles, LogicalCoreCount);
+  EndTick = TimerGetTick();
+  printf("seconds elapsed: %fs\n", TimerGetSecondsElepsed(BeginTick, EndTick));
+  printf("\n\n\n\n");
   
+  //GPU
   printf("parallel gpu c\n");
+  BeginTick = TimerGetTick();
   CudaRunCodeFromC();
+  EndTick = TimerGetTick();
+  printf("seconds elapsed: %fs\n", TimerGetSecondsElepsed(BeginTick, EndTick));
+  
+  //END
   printf("done\n");
   return;
 }
